9470b104f77bfd2acb4b17633efb694c4dbc6b57
samuel diaz <samueldiazkoc@gmail.com>
2020-09-10 09:22:10 -0500

    include schudle 2

---
+++ schudle2.php
@@ -0,0 +1,375 @@
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></script>
+
+<h1 id="principal">Disponibilidad</h1>
+<div class="wrapper">
+<div class="container">
+<br>
+<button type="button" class="btn btn-success">Registrar Disponibilidad</button>
+<br><br>
+<div class="btn-group btn-group-lg" role="group" aria-label="..."><button class="btn btn-primary"><i class="fa fa-list"></i> Listado</button>
+<button class="btn btn-primary"><i class="fa fa-calendar"></i> Horario</button>
+</div>
+<br><br>
+<h3 id="titulo">Horario</h3>
+<div class="col-md-12">
+
+<div class="row">
+    <h5 class="ml-3">Modalidades: </h5>
+    
+
+        
+        <a class="event  p-1 pl-2 pr-2 mb-3 ml-3 rounded text-truncate small bg-primary text-white text-center" >Animal Flow</a>
+        
+            <a class="event  p-1 pl-2 pr-2 mb-3 ml-3 rounded text-truncate small bg-success text- text-center" >Raw Strenght</a>
+           
+        
+             <a class="event  p-1 pl-2 pr-2 mb-3 ml-3 rounded text-truncate small bg-danger text-white text-center" >Clubbels</a>
+           
+        
+             <a class="event  p-1 pl-2 pr-2 mb-3 ml-3 rounded text-truncate small bg-info   text-white text-center" >Metabolic Circuit</a>
+           
+        </div>
+
+    
+<div class="row">
+<div class="col">
+<ul class="list-group" id="first-li">
+  <li class="list-group-item text-center">Horarios</li>
+
+  
+  <li class="list-group-item text-center list-group-item-light">06:00</li>
+  <li class="list-group-item text-center list-group-item-light">06:30</li>
+  <li class="list-group-item text-center list-group-item-light">07:00</li>
+  <li class="list-group-item text-center list-group-item-light">07:30</li>
+  <li class="list-group-item text-center list-group-item-light">08:00</li>
+  <li class="list-group-item text-center list-group-item-light">08:30</li>
+  <li class="list-group-item text-center list-group-item-light">09:00</li>
+  <li class="list-group-item text-center list-group-item-light">09:30</li>
+  <li class="list-group-item text-center list-group-item-light">10:00</li>
+  <li class="list-group-item text-center list-group-item-light">10:30</li>
+  <li class="list-group-item text-center list-group-item-light">11:00</li>
+  <li class="list-group-item text-center list-group-item-light">11:30</li>
+  <li class="list-group-item text-center list-group-item-light">12:00</li>
+  <li class="list-group-item text-center list-group-item-light">12:30</li>
+  <li class="list-group-item text-center list-group-item-light">13:30</li>
+  <li class="list-group-item text-center list-group-item-light">14:00</li>
+  <li class="list-group-item text-center list-group-item-light">14:30</li>
+  <li class="list-group-item text-center list-group-item-light">15:00</li>
+  <li class="list-group-item text-center list-group-item-light">15:30</li>
+  <li class="list-group-item text-center list-group-item-light">16:00</li>
+  <li class="list-group-item text-center list-group-item-light">16:30</li>
+  <li class="list-group-item text-center list-group-item-light">17:00</li>
+  <li class="list-group-item text-center list-group-item-light">17:30</li>
+  <li class="list-group-item text-center list-group-item-light">18:00</li>
+  <li class="list-group-item text-center list-group-item-light">18:30</li>
+  <li class="list-group-item text-center list-group-item-light">19:00</li>
+  <li class="list-group-item text-center list-group-item-light">19:30</li>
+  <li class="list-group-item text-center list-group-item-light">20:00</li>
+  <li class="list-group-item text-center list-group-item-light">20:30</li>
+  <li class="list-group-item text-center list-group-item-light">21:00</li>
+  <li class="list-group-item text-center list-group-item-light">21:30</li>
+  <li class="list-group-item text-center list-group-item-light">22:00</li>
+ 
+</ul>
+</div>
+<div class="col">
+<ul class="list-group">
+  <li class="list-group-item text-center">Lunes </li>
+
+  
+  <li class="list-group-item list-group-item-light">
+  <a class="event   rounded text-truncate p-1 bg-info   text-white text-center" >06:00-07:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event  rounded text-truncate p-1 bg-info   text-white text-center" >07:00-08:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event rounded text-truncate p-1  bg-info   text-white text-center" >08:00-09:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+   <li class="list-group-item list-group-item-light">
+  <a class="event   rounded text-truncate p-1 bg-info   text-white text-center" >06:00-07:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event  rounded text-truncate p-1 bg-info   text-white text-center" >07:00-08:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event rounded text-truncate p-1  bg-info   text-white text-center" >08:00-09:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  
+</ul>
+</div>
+<div class="col">
+<ul class="list-group">
+  <li class="list-group-item text-center">Martes</li>
+
+  
+ <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event   rounded text-truncate  bg-info p-1 text-white text-center" >07:00-10:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light"><a class="event    rounded text-truncate  bg-info  p-1 text-white text-center" >07:00-08:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event   rounded text-truncate  bg-info p-1  text-white text-center" >07:00-10:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event rounded text-truncate  bg-info p-1  text-white text-center" >07:00-10:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event   rounded text-truncate  bg-info p-1 text-white text-center" >07:00-10:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light"><a class="event    rounded text-truncate  bg-info  p-1 text-white text-center" >07:00-08:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event   rounded text-truncate  bg-info p-1  text-white text-center" >07:00-10:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">
+  <a class="event rounded text-truncate  bg-info p-1  text-white text-center" >07:00-10:00</a>
+  </li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+</ul>
+</div>
+<div class="col">
+<ul class="list-group">
+  <li class="list-group-item text-center">Miercoles</li>
+
+  
+<li class="list-group-item list-group-item-light"><a class="event rounded text-truncate  bg-info p-1 text-white text-center" >07:00-08:00</a></li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light"><a class="event rounded text-truncate  bg-info p-1 text-white text-center" >07:00-08:00</a></li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+</ul>
+</div>
+<div class="col">
+<ul class="list-group">
+  <li class="list-group-item text-center">Jueves</li>
+
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">
+  --
+  </li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">
+  --
+  </li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+</ul>
+</div>
+<div class="col">
+<ul class="list-group">
+  <li class="list-group-item text-center">Viernes</li>
+
+  
+<li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light"><a class="event  p-1 rounded text-truncate  bg-success   text-white text-center" >07:00-08:00</a></li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>  
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light"><a class="event  p-1 rounded text-truncate  bg-success   text-white text-center" >07:00-08:00</a></li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+</ul>
+</div>
+<div class="col">
+<ul class="list-group">
+  <li class="list-group-item text-center">Sabado  </li>
+
+  
+<li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light"><a class="event  p-1  rounded text-truncate  bg-success   text-white text-center" >08:00-09:00</a></li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>  
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light"><a class="event  p-1  rounded text-truncate  bg-success   text-white text-center" >08:00-09:00</a></li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+  <li class="list-group-item list-group-item-light">--</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+
+<style>
+    body{
+        background-color: #e6e9ed;
+    }
+    #principal{
+        margin-top: 20px;
+        margin-left: 150px;
+    }
+    .col{
+      padding: 0;
+      position: relative;
+    }
+   
+    .wrapper{
+        background-color: white;
+        margin:30 150px;
+        min-height: 500px;
+    }
+    #titulo{
+        margin-left: 10px;
+    }    
+  
+    
+   
+
+    
+</style>
